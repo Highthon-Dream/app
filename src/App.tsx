dfd8854e9bef47daf14783385b74f691db483e3b
J1min <aoiharu41@gmail.com>
2024-02-17 21:27:11 +0900

    다함

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import SignUp from "./pages/SignUp";
 import ToastEditor from "./pages/BucketListWrite";
 import TodoWrite from "./pages/TodoWrite";
 import Detail from "./pages/Detail";
+import BucketListDetailView from "./pages/BucketListDetailView";
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
         <Route path="/" element={<Main />} />
         <Route path="/bucket" element={<Buket />} />
         <Route path="/bucket/write/:id" element={<Detail />} />
+        <Route path="/bucket/:id" element={<BucketListDetailView />} />
         <Route path="/write" element={<ToastEditor />} />
         <Route path="/bucket/write" element={<TodoWrite />} />
         <Route path="/profile" element={<Profile />} />
