aca18bcf4436e46ad0c9d8f91208506154615c31
J1min <aoiharu41@gmail.com>
2024-02-17 20:00:11 +0900

    통신추가

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,6 +1,13 @@
+import { useEffect } from "react";
+import httpClient from "../apis/httpClient";
 import Leftbar from "../components/Leftbar";
 
 export default function Main() {
+  useEffect(() => {
+    httpClient.bucketList.get().then((r) => {
+      console.log(r.data);
+    });
+  }, []);
   return (
     <div className="flex items-start">
       <Leftbar />
