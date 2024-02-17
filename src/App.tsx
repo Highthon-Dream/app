cfe0264180a0fb8f746e4585bf253244950a6948
이운린 <105215297+likegitman@users.noreply.github.com>
2024-02-17 19:32:38 +0900

    Add Header Leftbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
+import Header from "./components/Header";
+import Leftbar from "./components/Leftbar";
+import Buket from "./pages/Buket";
 import Main from "./pages/Main";
 import { Route, Routes } from "react-router-dom";
+import Profile from "./pages/Profile";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Main />} />
-      <Route path="/test" element={<Main />} />
-    </Routes>
+    <div>
+      <Header />
+      <div className="flex items-start">
+        <Leftbar />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/buket" element={<Buket />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </div>
+    </div>
   );
 };
 export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { useLocation } from "react-router-dom";
+
+const Header = () => {
+  const { pathname } = useLocation();
+  return (
+    <>
+      {pathname === "/login" || pathname === "/signup" ? (
+        <></>
+      ) : (
+        <nav className="size-full bg-primary h-[100px] text-white text-[40px] font-['LOTTERIACHAB'] p-8 flex items-center">
+          <div className="bg-white text-primary text-[30px] w-[168px] h-[64px] rounded-md flex items-center justify-center">
+            <h1>WEEGO</h1>
+          </div>
+          <h1 className="m-10">나에게로 떠나는 여행</h1>
+        </nav>
+      )}
+    </>
+  );
+};
+
+export default Header;
