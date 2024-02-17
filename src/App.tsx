58adf814594dfc113b86d636a58a26afea3d0c79
J1min <aoiharu41@gmail.com>
2024-02-17 18:57:03 +0900

    로그인 회원가입 페이지 추가, httpClient 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import Login from "./pages/Login";
 import Main from "./pages/Main";
 import { Route, Routes } from "react-router-dom";
+import SignUp from "./pages/SignUp";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
       <Route path="/test" element={<Main />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
     </Routes>
   );
 };
