dd5ed0c13ab0aca31ea55df2dd2857669b0ac0b6
J1min <aoiharu41@gmail.com>
2024-02-17 18:28:40 +0900

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-function App() {
-  return (
-    <div className="App">
-      <div className="text-red-500">ddd</div>
-    </div>
-  );
-}
+import Main from "./pages/Main";
+import { Route, Routes } from "react-router-dom";
 
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="/test" element={<Main />} />
+    </Routes>
+  );
+};
 export default App;
