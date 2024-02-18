07ff9425e8e202120c0be13722b2d3c9838a307f
이운린 <105215297+likegitman@users.noreply.github.com>
2024-02-18 09:51:07 +0900

    Update Logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,9 +9,7 @@ const Header = () => {
         <></>
       ) : (
         <nav className="size-full bg-primary h-[100px] text-white text-[40px] font-['LOTTERIACHAB'] p-8 flex items-center sticky top-0 z-[999]">
-          <div className="bg-white text-primary text-[30px] w-[168px] h-[64px] rounded-md flex items-center justify-center">
-            <h1>WEEGO</h1>
-          </div>
+          <img src="/Logo.png" />
           <h1 className="m-10">나에게로 떠나는 여행</h1>
         </nav>
       )}
